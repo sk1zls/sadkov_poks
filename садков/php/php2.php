6b9dc73d849a4a8536162f08bd74b78d4aeff32a
sk1zls <130976550+sk1zls@users.noreply.github.com>
2023-09-16 08:56:39 +0300

    Add files via upload

---
+++ садков/php/php2.php
@@ -0,0 +1,7 @@
+<?php
+define('PASSWORD', 'любое_значение'); // Объявление константы PASSWORD
+define('PI', 3.14, true); // Объявление регистронезависимой константы PI
+
+echo 'Значение PASSWORD: ' . PASSWORD . "\n";
+echo 'Значение PI: ' . PI . "\n";
+?>
---
+++ садков/php/php4.php
@@ -0,0 +1,16 @@
+<?php
+$array = [
+    "php" => "PHP users guide",
+    12 => true
+];
+
+$array["Book about Perl"] = ""; // Создаем элемент с неуказанным индексом
+
+$array["lisp"] = 123456; // Добавляем пару "lisp" = 123456
+
+unset($array[12]); // Удаляем элемент с ключом 12
+
+$array = array_values($array); // Переиндексируем массив с помощью array_values
+
+print_r($array); // Выводим массив на экран
+?>
