5254a2ba49f4f21cdea14b56e1814300d85bde4e
sk1zls <130976550+sk1zls@users.noreply.github.com>
2023-09-23 09:18:17 +0300

    Delete садков directory

--- садков/php/php2.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PASSWORD', 'любое_значение'); // Объявление константы PASSWORD
-define('PI', 3.14, true); // Объявление регистронезависимой константы PI
-
-echo 'Значение PASSWORD: ' . PASSWORD . "\n";
-echo 'Значение PI: ' . PI . "\n";
-?>
